c48793f0c405a76d1065abe86053bdd015879abe
fduxiao <xiao.tan@qianpuinvestment.com>
2018-03-14 23:29:37 +0800

    add simple electron front end

---
+++ event.js
@@ -0,0 +1,76 @@
+let server;
+let shell = require('electron').shell;
+let app = get_express_server();
+
+function serve(path) {
+    if(server) {
+        server.close()
+    }
+    try {
+        let certs = require(path);
+        set_api_server(app, certs);
+        let port = $("#port").val();
+        port = parseInt(port);
+        server = app.listen(port)
+    } catch(e) {
+        alert(e);
+    }
+}
+
+$(document).ready(()=>{
+  let path2 = localStorage.getItem('path');
+    if(path2) {
+        $("#user-file-path").val(path2)
+    }
+    $("#port").val(3001);
+
+    $("#user-file").change(() => {
+      let files = $("#user-file").prop('files');
+        if(files.length !== 0) {
+            $("#user-file-path").val(files[0].path)
+        }
+    });
+
+    $("#open-user-file").click(()=>{
+        $("#user-file").click()
+    });
+
+    $('#serve').click(()=>{
+        let path = $("#user-file-path").val();
+        localStorage.setItem('path', path);
+        if(!path) {
+            alert('please select a user identity');
+            return
+        }
+        serve(path)
+    });
+
+    $("#stop").click(() => {
+        if(server) {
+            server.close();
+            server = undefined
+        }
+    });
+
+    $("#open-link").click(() => {
+        if(!server) {
+            alert('please launch the server');
+            return
+        }
+        let port = $("#port").val();
+        // window.open("http://localhost:"+port+'/api/user/info')
+        mainView2("http://localhost:"+port+'/api/user/info')
+    });
+    $("#open-extra").click(() => {
+        if(!server) {
+            alert('please launch the server');
+            return
+        }
+        let port = $("#port").val();
+        shell.openExternal("http://localhost:"+port+'/api/user/info')
+    });
+    function mainView2(url) {
+        let webview = document.getElementById('main-view');
+        webview.loadURL(url);
+    }
+});
